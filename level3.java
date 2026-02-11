8f48627d2a60e0ec8a4a9c569487ce5789aabea8
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-02-11 22:37:37 +0530

    Create level3.java
    Variable and Data types

---
+++ level3.java
@@ -0,0 +1,14 @@
+// Variables and Data Types
+public class level3 {
+    public static void main(String[] args) {
+       int x=2;
+       int y=4;
+       int sum=x+y;
+     int c=x*y/x+y;
+        System.out.println("The value of c is: "+c);
+       int prdt=x*y;
+       System.out.println("The sum of x and y is: "+sum); 
+         System.out.println("The product of x and y is: "+prdt);
+    }
+}
+  
