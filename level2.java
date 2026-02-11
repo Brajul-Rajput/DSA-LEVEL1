eae55dbeff7ca460aae07da5dfcc97df344162f3
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-02-11 22:08:57 +0530

    Create level2.java
    How to print Z

---
+++ level2.java
@@ -0,0 +1,11 @@
+// How to print Z
+public class level2 {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("        *");
+        System.out.println("      *");
+        System.out.println("    *");
+        System.out.println("  *");
+        System.out.println("* * * * *");
+    }
+}
