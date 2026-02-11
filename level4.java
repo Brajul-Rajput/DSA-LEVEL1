05ce75ec7b5fe4541477f08fafc4c019f33062d2
Brajul-Rajput <132092432+Brajul-Rajput@users.noreply.github.com>
2026-02-11 22:38:03 +0530

    Create level4.java
    IF ELSE-IF  ELSE PROGRAM IN JAVA

---
+++ level4.java
@@ -0,0 +1,18 @@
+//IF ELSE-IF  ELSE PROGRAM IN JAVA
+ 
+public class level4 {
+    public static void main(String[] args) {
+        int a=1;
+        int b=0;
+        int c=3;
+        if(a>b && a>c){
+            System.out.println("a is the greatest number");
+        }
+        else if(b>a && b>c){
+            System.out.println("b is the greatest number");
+        }
+        else{
+            System.out.println("c is the greatest number");
+        }
+    }
+}
